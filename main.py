9f3c7552db1b74a786d1c75097f5984a66d23d4e
PelicanQ <77930606+PelicanQ@users.noreply.github.com>
2024-12-06 11:56:40 +0100

    generates fractal

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
 # Python code to implement Conway's Game Of Life
 import matplotlib.axes
+import matplotlib.image
 import matplotlib.lines
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
+import scipy.spatial
 import rules
 from const import ON, OFF
 import cluster
 from scipy.ndimage import convolve
+import scipy
 import matplotlib
-import multiprocessing as mp
-import time
 
-# import numba
 
 # setting up the values for the grid
 
@@ -25,7 +25,6 @@ def randomGrid(N, p):
 
 
 def start(N: int):
-    """returns a grid of NxN random values"""
     if N % 2 == 1:
         raise Exception("N must be even")
     a = np.zeros((N, N), dtype=np.int16)
@@ -43,25 +42,82 @@ def start(N: int):
     return a
 
 
-chosen_rule = rules.fig6
+def start2(N: int):
+    if N % 2 == 1:
+        raise Exception("N must be even")
+    a = np.zeros((N, N), dtype=np.int16)
+    m = int(N / 2) + 2
+    a[m, m] = ON
+    a[m + 1, m] = ON
+    a[m - 1, m] = ON
+    a[m, m + 1] = ON
+    a[m + 1, m + 1] = ON
+    a[m - 1, m + 1] = ON
+    a[m, m - 1] = ON
+    a[m + 1, m - 1] = ON
+    a[m - 1, m - 1] = ON
+
+    # pertubation
+    # a[m - 2, m - 1] = ON
+    # a[m - 2, m] = ON
+    return a
+
+
+def start3(N: int):
+    if N % 2 == 1:
+        raise Exception("N must be even")
+    a = np.zeros((N, N), dtype=np.int16)
+    m = int(N / 2) + 2
+    a[m - 0, m + 1] = ON
+    a[m - 1, m] = ON
+    a[m - 2, m] = ON
+    a[m - 3, m + 1] = ON
+    a[m - 2, m + 2] = ON
+    a[m - 1, m + 2] = ON
+
+    # pertub
+    a[m - 0, m] = ON
+    a[m, m - 1] = ON
+    return a
+
+
+def start4(N: int):
+    if N % 2 == 1:
+        raise Exception("N must be even")
+    a = np.zeros((N, N), dtype=np.int16)
+    m = int(N / 2) + 2
+    a[m - 0, m + 1] = ON
+    a[m - 1, m] = ON
+    a[m - 2, m] = ON
+    a[m - 3, m + 1] = ON
+    a[m - 2, m + 2] = ON
+    a[m - 1, m + 2] = ON
+
+    # pertub
+    a[m - 0, m] = ON
+    a[m + 1, m - 1] = ON
+    return a
+
+
+chosen_rule = rules.conway
 rounds = 100
 fractionsX = np.zeros((rounds, 1))
 fractionsY = np.zeros((rounds, 1))
 scatter: matplotlib.axes.Axes
 
 
-def _update_grid_pos(grid, i: int, j: int, num_neighbors: int) -> None:
-    print("HEJ", type(i))
-    i = int(i)
-    j = int(j)
-    grid[i, j] = chosen_rule(grid, i, j, num_neighbors)
+birth_map = [OFF, OFF, OFF, ON, OFF, OFF, OFF, OFF, OFF]  # if dead
+death_map = [OFF, OFF, ON, ON, OFF, OFF, OFF, OFF, OFF]  # if alive
 
 
-birth_map = [OFF, OFF, OFF, ON, OFF, OFF, ON, OFF, OFF]  # if dead
-death_map = [OFF, OFF, ON, ON, OFF, ON, ON, OFF, OFF]  # if alive
-
-
-def update(frame: int, grid, img=None, parallel=False):
+def update(
+    grid,
+    frame: int | None = None,
+    img=None,
+    parallel=False,
+    line: matplotlib.lines.Line2D | None = None,
+):
+    """Returns grid"""
     kernel = np.array([[1, 1, 1], [1, 0, 1], [1, 1, 1]], dtype=np.int16)
     conv = convolve(grid, kernel, mode="wrap")
     num_neighbors = np.floor_divide(conv, 255)
@@ -75,18 +131,6 @@ def update(frame: int, grid, img=None, parallel=False):
     # fractionsX[frame] = np.sum(grid) / (N * M * ON)  # set prev fraction
 
     # update grid according to global chosen_rule
-    # if parallel:
-    #     shared_array = mp.Array("d", N * M)
-    #     for i in range(N):
-    #         for j in range(M):
-    #             proc = mp.Process(
-    #                 target=_update_grid_pos,
-    #                 args=(shared_array, i, j, int(num_neighbors[i, j])),
-    #             )
-    #             proc.start()
-    #     grid = np.frombuffer(shared_array.get_obj()).reshape((N, M))
-
-    # else:
 
     for i in range(N):
         for j in range(M):
@@ -95,58 +139,21 @@ def update(frame: int, grid, img=None, parallel=False):
             else:
                 grid[i, j] = death_map[num_neighbors[i, j]]
             # grid[i, j] = chosen_rule(grid, i, j, int(num_neighbors[i, j]))
-
-    # if img:
-    # img.set_data(grid)
+    return grid
+    if img:
+        img.set_data(grid)
+    if line:
+        b = line.get_data()
+        n0 = np.append(b[0], frame)
+        n1 = np.append(b[1], hamming(grid))
+        line.set_data(n0, n1)
+        print(line.get_data())
 
     # fractionsY[frame] = np.sum(grid) / (N * M * ON)  # set updated fraction
     # scatter.scatter(fractionsX[frame], fractionsY[frame])
 
 
-def _update(_: int, grid, N: int, activity, img=None, act_img=None, fractions=None):
-    """
-    Returns: fraction of alive cells in the updated state
-    """
-    # copy grid since we require 8 neighbors
-    # for calculation and we go line by line
-    newGrid = grid.copy()
-    for i in range(N):
-        for j in range(N):
-
-            # compute 8-neighbor sum
-            # using toroidal boundary conditions - x and y wrap around
-            # so that the simulation takes place on a toroidal surface.
-            total = int(
-                (
-                    grid[i, (j - 1) % N]
-                    + grid[i, (j + 1) % N]
-                    + grid[(i - 1) % N, j]
-                    + grid[(i + 1) % N, j]
-                    + grid[(i - 1) % N, (j - 1) % N]
-                    + grid[(i - 1) % N, (j + 1) % N]
-                    + grid[(i + 1) % N, (j - 1) % N]
-                    + grid[(i + 1) % N, (j + 1) % N]
-                )
-                / 255
-            )
-
-            # apply  rules
-
-            # conway(i, j, total, grid, newGrid, activity)
-            chosen_rule(i, j, total, grid, newGrid, activity)
-
-    # update data
-    grid[:] = newGrid[:]
-    if img:
-        img.set_data(newGrid)
-    if act_img:
-        act_img.set_data(activity)
-
-    return np.sum(grid) / (N * N * ON)
-
-
 def update_nowrap(frame, img, grid, N, activity, act_img):
-    print(frame)
     # copy grid since we require 8 neighbors
     # for calculation and we go line by line
     newGrid = grid.copy()
@@ -254,41 +261,140 @@ def update_nowrap(frame, img, grid, N, activity, act_img):
     grid[:] = newGrid[:]
 
 
-# main() function
+# def hamming(grid: np.ndarray, grid2: np.ndarray | None = None):
+#     # calculate hamming distance to zero for now
+#     a = grid.flatten()
+#     d = scipy.spatial.distance.hamming(a, b)
+#     return d
+def num2grid(N: int, n: int):
+    """n has to be 0 <= n < N*N"""
+    a = np.zeros((N * N,), dtype=np.int16)
+    for i, bit in enumerate(bin(n)[2:].zfill(N * N)):
+        a[i] = int(bit) * ON
+    return np.reshape(a, newshape=(N, N))
 
 
 def main():
+    N = 16  # grid size
+    FRAMES = 20  # how many updates for one run
+    RUNS = 10  # how many inital conditions
+    updateInterval = 300  # milliseconds animation update
+    rs = np.zeros((RUNS, FRAMES))  # real number encoding state
 
-    N = 300  # grid size
+    # grid = start3(N)
+    # grid2 = start4(N)
+    # for run in range(RUNS):
+    # for frame in range(FRAMES):
+    X = []
+    Y = []
+    ending = 2**17
+    for start_n in range(ending):
+        print(f"{(start_n / ending):.3f}")
+        grid = num2grid(N=N, n=start_n)
+        new1 = update(grid=grid)
+        flat1 = new1.flatten()
+        r = 0
+        for i, digit in enumerate(flat1):
+            n = (digit // 255) * pow(2, -i)
+            r += n
+        X.append(start_n)  # where we started
+        Y.append(r)  # where update got us
 
-    updateInterval = 5  # milliseconds animation update
+    print("DONE")
+    # Y = rs[1:]
+    # X = rs[:-1]
+    ax = plt.subplot(111)
+    ax.plot(X, Y, marker=".")
+    ax.set_title("First order return map")
+    ax.set_xlabel("y_n")
+    ax.set_ylabel("y_n+1")
+    # ax.set_xscale("log")
+    # ax.set_yscale("log")
+    plt.show()
+    # inits plots
+    # fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(ncols=2, nrows=2)  # figure for CA grid
 
-    # grid = randomGrid(N, 0.2)
-    grid = start(N)
+    # img: matplotlib.image.AxesImage = ax1.imshow(grid, interpolation="nearest")
+    # img2: matplotlib.image.AxesImage = ax3.imshow(grid2, interpolation="nearest")
 
-    # set up animation
-    fig, axs = plt.subplots(ncols=2)  # figure for CA grid
+    # dist: matplotlib.axes.Axes = ax2
+    # dist2: matplotlib.axes.Axes = ax4
+    # [hamming_line] = dist.plot(0, 0, label="Hamming", marker=".", color="b")
+    # [jac_line] = dist.plot(0, 0, marker=".", color="r", label="Jaccard")
+    # [dice_line] = dist.plot(0, 0, marker=".", color="g", label="Dice")
+    # [kul_line] = dist.plot(0, 0, marker=".", color="b", label="Kul")
+    # [line1] = dist.plot(0, 0, marker=".", color="y", label="Line1")
+    # [line2] = dist.plot(0, 0, marker=".", label="Line2")
+    # [line3] = dist.plot(0, 0, marker=".", label="Line3")
+    # # [line4] = dist2.plot(0, 1, marker=".", label="Line4")
+    # # [map_line] = dist2.plot()
+    # dist.set_xlim([0, FRAMES])
+    # dist.legend()
 
-    img = axs[0].imshow(grid, interpolation="nearest")
-    global scatter
-    scatter = axs[1]
-    t1 = time.perf_counter()
-    for i in range(100):
-        update(i, grid, N, parallel=False)
-    t2 = time.perf_counter()
-    print(t2 - t1)
-    # prev = frac
-    # act_img.set_data(activity)
-    # img.set_data(grid)
-    # a = cluster.find_clusters(grid)
-    # u = np.unique(a, return_counts=True)
-    # axs[1].loglog(u[0], u[1])
     # anim = animation.FuncAnimation(
-    #     fig,
-    #     lambda frame: update(frame, grid, parallel=False, img=img),
-    #     interval=updateInterval,
-    # )
+    #         fig,
+    #         update_all,
+    #         interval=updateInterval,
+    #         frames=FRAMES,
+    #         repeat=False,
+    #     )
     # plt.show()
+    # def one_run():
+
+    #     img.set_data(grid)
+    #     img2.set_data(grid2)
+
+    #     def add_linedata(line, flat1, flat2, dist_func, frame):
+    #         data = line.get_data()
+    #         dist = dist_func(flat1, flat2)
+    #         log = np.append(data[1], dist)
+    #         line.set_data(np.append(data[0], frame), log)
+
+    #     def update_all(frame: int):
+    # new1 = update(frame, grid, parallel=False, img=img)
+    # new2 = update(frame, grid2, parallel=False, img=img)
+    # img.set_data(new1)
+    # img2.set_data(new2)
+
+    # flat1 = new1.flatten()
+    # flat2 = new2.flatten()
+    # r = 0
+    # for i, digit in enumerate(flat1):
+    #     n = (digit // 255) * pow(2, -i)
+    #     r += n
+
+    # rs[frame] = r
+    # # print(rs)
+    # add_linedata(
+    #     hamming_line, flat1, flat2, scipy.spatial.distance.hamming, frame
+    # )
+    # add_linedata(jac_line, flat1, flat2, scipy.spatial.distance.jaccard, frame)
+    # add_linedata(
+    #     dice_line,
+    #     flat1,
+    #     flat2,
+    #     lambda a, b: np.abs(scipy.spatial.distance.dice(a, b)),
+    #     frame,
+    # )
+    # add_linedata(
+    #     kul_line, flat1, flat2, scipy.spatial.distance.kulczynski1, frame
+    # )
+    # add_linedata(
+    #     line1, flat1, flat2, scipy.spatial.distance.rogerstanimoto, frame
+    # )
+    # add_linedata(line2, flat1, flat2, scipy.spatial.distance.russellrao, frame)
+    # add_linedata(
+    #     line3, flat1, flat2, scipy.spatial.distance.sokalmichener, frame
+    # )
+    # add_linedata(line4, flat1, flat2, scipy.spatial.distance.sokalsneath, frame)
+
+    # print(np.max(X), np.min(X))
+    # dist.relim()
+    # dist.autoscale_view()
+    # dist2.relim()
+    # dist2.autoscale_view()
+    # if frame == FRAMES - 1:
+    # done()
 
 
 # call main
